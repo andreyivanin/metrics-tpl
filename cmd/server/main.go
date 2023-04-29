e34108723c4b3f28d78f159e77186d5413f7e307
Andrey Ivanin <andrey.ivanin@gmail.com>
2023-04-29 20:59:49 +0300

    iter1 is done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"metrics-tpl/internal/server/handler"
+	"metrics-tpl/internal/server/storage"
+)
+
+func main() {
+	storage := storage.New()
+	customHandler := handler.NewHandler(storage)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", customHandler.MetricUpdate)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Println(err)
+	}
+}
